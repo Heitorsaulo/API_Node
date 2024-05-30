ce441562c67566c8d2cc1e41c6694f6b6d0fc71c
Heitor Saulo <heitor-081@outlook.com>
2024-05-30 12:11:36 -0300

    adicionando metodo Update de Usuario

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API");
 })
 
+//resgatar todos os usuario do banco
 app.get('/api/usuarios', async (req, res) => {
     try {
         const usuarios = await Usuario.find({});
@@ -21,6 +22,7 @@ app.get('/api/usuarios', async (req, res) => {
     }
 });
 
+//resgatar usuario pelo id
 app.get('/api/usuario/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -32,10 +34,28 @@ app.get('/api/usuario/:id', async (req, res) => {
     }
 });
 
+//inserir usuario no banco
 app.post('/api/usuario', async (req, res) => {
     try {
         const usuario = await Usuario.create(req.body);
         res.status(200).json(usuario)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+//atualizar os dados de um usuario
+app.put('/api/usuario/:id', async (req,res) =>{
+    try {
+        const { id } = req.params;
+
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+
+        if(!usuario){
+            return res.status(404).json({message: "usuario não encontrado"});
+        }
+        const usuarioAtualizado = await Usuario.findById(id);
+        res.status(200).json(usuarioAtualizado);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
